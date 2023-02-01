11d11f87fceaddaea8ae22f59b79a5bae9366e3b
VenoM9078 <roushan.venom1@gmail.com>
2023-02-01 14:35:24 +0500

    Fixed Bg

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -46,7 +46,7 @@ class RegisteredUserController extends Controller
         event(new Registered($user));
 
         Auth::login($user);
-        return redirect('/home');
+        return redirect('/');
         // return redirect(RouteServiceProvider::HOME);
     }
 }
